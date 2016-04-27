5c0d653e29a4bc8150dd4e23472c22d070d68308
jrojas2016 <jrojas2016@gmail.com>
2016-04-26 23:33:55 -0400

    Main script structure. Multi-threading implemented to fetch access token for spotify API.

---
+++ musicClassifier.py
@@ -0,0 +1,29 @@
+'''
+Music Classification by Genre:
+	Description
+
+Date: 4/26/2016
+
+Author(s): 
+	Jorge Rojas
+'''
+import csv
+import thread
+import neuralNet as NN
+import spotifyAuth as sa
+
+def createNetwork(createFile):
+	''' TRAIN DATA PROCESSING '''
+
+	''' NEURAL NETWORK '''
+
+def main():
+
+	try:
+		thread.start_new_thread(createNetwork, (create_file, ) )
+	except:
+		print "Unable to start thread!"
+	sa.runAuth()
+
+if __name__ == '__main__':
+	main()
